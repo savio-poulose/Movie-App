310afd2ccfab15c253ac3a4629efc2e0a1809f80
Savio Poulose <saviopoulose05@gmail.com>
2026-08-28 20:35:57 +0530

    wasnt working while hosting

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,15 +1,23 @@
-const API_KEY = "e8fab775ac2d71ab3f34de57d73c9124"
-const BASE_URL = "http://api.themoviedb.org/3"
+const API_KEY = "e8fab775ac2d71ab3f34de57d73c9124";
 
-export const getPopularMovies = async ()=>{
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
-    const data = await response.json()
-    return data.results
-}
+const BASE_URL = "https://api.themoviedb.org/3";
 
-export const searchMovie = async (query)=>{
-    const response = await fetch(
-         `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
-    const data = await response.json()
-    return data.results
-}
+export const getPopularMovies = async () => {
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+export const searchMovie = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+  );
+
+  const data = await response.json();
+
+  return data.results;
+};
